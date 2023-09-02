97dd149516cb27bac265cb3a4afd9de3db8b8a61
aynorway <adrianynorway@gmail.com>
2023-09-01 20:07:30 -0400

    feat: Implement audio notification upon timer completion

--- TimeTrackBar.java
+++ TimeTrackBar.java
@@ -1,7 +1,14 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URL;
 
 
 public class TimeTrackBar {
@@ -41,7 +48,13 @@ public class TimeTrackBar {
         private JLabel remainingTimeLabel;
         private int duration;
         private Timer timer;
-        private JPanel timeInputPanel;  // 新增的面板，用于容纳时间设置部分
+        private JPanel timeInputPanel; // 新增的面板，用于容纳时间设置部分
+        private JButton soundToggleButton; // 警报声音按钮
+        private JButton startButton;
+        private boolean isTimerRunning = false;
+        private Clip clip;
+        private boolean isSoundEnabled = true;  // 初始状态不静音
+
 
         public TimerTaskPanel(boolean isFirst) {
             super(new BorderLayout());
@@ -80,7 +93,7 @@ public class TimeTrackBar {
 
             // 定义新的timeInputPanel
             timeInputPanel = new JPanel();
-            daysField = new JTextField(2);  
+            daysField = new JTextField(2);
             timeInputPanel.add(daysField);
             timeInputPanel.add(new JLabel("d"));
             hoursField = new JTextField(2);
@@ -93,25 +106,51 @@ public class TimeTrackBar {
             timeInputPanel.add(secondsField);
             timeInputPanel.add(new JLabel("s"));
             eastPanel.add(timeInputPanel);
-            
 
             remainingTimeLabel = new JLabel("0d 0h 0m 0s");
-            JButton startButton = new JButton("▶");
+            startButton = new JButton("▶");
             startButton.setForeground(Color.GREEN);
             startButton.setPreferredSize(new Dimension(40, 30));
             startButton.addActionListener(e -> startCountdown());
 
-            JButton toggleButton = new JButton("⏲");  // 使用时钟字符，用于显示/隐藏时间设置部分
+            JButton toggleButton = new JButton("⏲"); // 使用时钟字符，用于显示/隐藏时间设置部分
             toggleButton.setPreferredSize(new Dimension(40, 30));
             toggleButton.addActionListener(e -> {
                 timeInputPanel.setVisible(!timeInputPanel.isVisible());
-                TimeTrackBar.this.mainFrame.revalidate();  // 确保面板重新布局
+                TimeTrackBar.this.mainFrame.revalidate(); // 确保面板重新布局
             });
             eastPanel.add(toggleButton);
+
+            soundToggleButton = new JButton("\u266B"); // 默认为有声音的状态
+            Font emojiFont = new Font("Apple Color Emoji", Font.PLAIN, 12); // For macOS
+            soundToggleButton.setFont(emojiFont);
+            System.out.println(soundToggleButton.getFont());
+
+            soundToggleButton.setPreferredSize(new Dimension(40, 30));
+            soundToggleButton.addActionListener(e -> toggleSound());
+            eastPanel.add(soundToggleButton);
 
             eastPanel.add(remainingTimeLabel);
             eastPanel.add(startButton);
             add(eastPanel, BorderLayout.EAST);
+
+        }
+
+        private void toggleSound() {
+            if (clip != null && clip.isActive()) { // 如果音乐正在播放
+                clip.stop();
+                soundToggleButton.setText("\u266B");
+                soundToggleButton.setBackground(null);
+            } else {
+                isSoundEnabled = !isSoundEnabled;
+                if (isSoundEnabled) {
+                    soundToggleButton.setText("\u266B");
+                    soundToggleButton.setBackground(null);
+                } else {
+                    soundToggleButton.setText("\u263D");
+                    soundToggleButton.setBackground(Color.RED);
+                }
+            }
         }
 
         private void startCountdown() {
@@ -148,16 +187,57 @@ public class TimeTrackBar {
                     remainingTime %= 60 * 60;
                     int minutesLeft = remainingTime / 60;
                     int secondsLeft = remainingTime % 60;
-                    remainingTimeLabel.setText(daysLeft + "d " + hoursLeft + "h " + minutesLeft + "m " + secondsLeft + "s");
+                    remainingTimeLabel
+                            .setText(daysLeft + "d " + hoursLeft + "h " + minutesLeft + "m " + secondsLeft + "s");
                 } else {
                     ((Timer) e.getSource()).stop();
+                    if (isSoundEnabled) {
+                        playAlarmSound();
+                    }
                 }
             });
-            timer.start();
 
-            printSizes(); 
+            timer.start();
+            isTimerRunning = true;
+            startButton.setText("⏹"); // ⏹ 是停止符号
+            startButton.setForeground(Color.RED);
+            startButton.removeActionListener(startButton.getActionListeners()[0]); // 移除旧的监听器
+            startButton.addActionListener(e -> stopCountdown()); // 添加一个新的监听器来停止计时
+
+            printSizes();
+        }
+
+        private void stopCountdown() {
+            if (timer != null) {
+                timer.stop();
+            }
+            if (clip != null) {
+                clip.stop();
+            }
+            isTimerRunning = false;
+            startButton.setText("▶");
+            startButton.setForeground(Color.GREEN);
+            startButton.removeActionListener(startButton.getActionListeners()[0]); // 移除旧的监听器
+            startButton.addActionListener(e -> startCountdown()); // 添加回原始的监听器
+        }
+
+        private void playAlarmSound() {
+            try {
+                URL audioURL = TimeTrackBar.class.getResource("/timbre_whaaat.wav");
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioURL);
+                clip = AudioSystem.getClip();
+                clip.open(audioStream);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(mainFrame, "无法播放声音。");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(mainFrame, "发生了其他错误。");
+            }
         }
         
+
     }
 
     private void printSizes() {
@@ -184,6 +264,7 @@ public class TimeTrackBar {
                 System.out.println("duration Value: " + timerTaskPanel.duration);
             }
         }
+
     }
 
     public static void main(String[] args) {
